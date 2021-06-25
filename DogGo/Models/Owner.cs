8d7dae9599a0a55aed203c67422cd84b012d6c4c
Sidney Crandall <sdncrandall5@gmail.com>
2021-06-25 14:19:11 -0500

    Attributes added

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DogGo.Models
 {
     public class Owner
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(55, MinimumLength = 5)]
         public string Address { get; set; }
+
+        [Phone]
+        [DisplayName("Phone Number")]
         public string Phone { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
+
         // Similar to the Walker model, let's grab the Foreign Entity of the Neighborhood, for the future.
         public Neighborhood Neighborhood { get; set; }
         // Lets attach Dogs to the owners as well
